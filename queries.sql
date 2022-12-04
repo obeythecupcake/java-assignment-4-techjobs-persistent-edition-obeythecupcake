3c6835b846bca6b027b8a100e7fddbc0e9816033
Denise Paniagua <obeythecupcake@gmail.com>
2022-12-03 23:03:02 -0600

    Part 2 done!

--- queries.sql
+++ queries.sql
@@ -1,7 +1,7 @@
 -- Part 1: Test it with SQL
 -- id: INT, employer: VARCHAR, name: VARCHAR, skills: VARCHAR --
 -- Part 2: Test it with SQL
-
+--SELECT name FROM employer WHERE location = "St. Louis City";
 -- Part 3: Test it with SQL
 
 -- Part 4: Test it with SQL
