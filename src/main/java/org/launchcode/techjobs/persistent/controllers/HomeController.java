ccc3531ce881a7f91778456550e5ee2977da63fb
Denise Paniagua <obeythecupcake@gmail.com>
2022-12-04 20:02:57 -0600

    everythig done but working on debugging now

--- src/main/java/org/launchcode/techjobs/persistent/controllers/HomeController.java
+++ src/main/java/org/launchcode/techjobs/persistent/controllers/HomeController.java
@@ -5,6 +5,7 @@ import org.launchcode.techjobs.persistent.models.Job;
 import org.launchcode.techjobs.persistent.models.Skill;
 import org.launchcode.techjobs.persistent.models.data.EmployerRepository;
 import org.launchcode.techjobs.persistent.models.data.JobRepository;
+import org.launchcode.techjobs.persistent.models.data.SkillRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -26,6 +27,9 @@ public class HomeController {
 
     @Autowired
     private JobRepository jobRepository;
+
+    @Autowired
+    private SkillRepository skillRepository;
 
 
     @RequestMapping("")
@@ -56,16 +60,18 @@ public class HomeController {
         if (optionalEmployer.isPresent()) {
             Employer employer = (Employer) optionalEmployer.get();
             newJob.setEmployer(employer);
-        } else { Employer employer = new Employer();
+        } else {
+            Employer employer = new Employer();
         newJob.setEmployer(employer);
         }
+        List<Skill> skillObjs = (List<Skill>) skillRepository.findAllById(skills);
+        newJob.setSkills(skillObjs);
         jobRepository.save(newJob);
         return "redirect:";
     }
 
     @GetMapping("view/{jobId}")
     public String displayViewJob(Model model, @PathVariable int jobId) {
-
         return "view";
     }
 
--- src/main/java/org/launchcode/techjobs/persistent/models/Job.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Job.java
@@ -1,25 +1,21 @@
 package org.launchcode.techjobs.persistent.models;
 
 import javax.persistence.*;
+import java.util.List;
 
 @Entity
 public class Job extends AbstractEntity{
 
-//    @Id
-//    @GeneratedValue
-//    private int id;
-//
-//    private String name;
-
     @ManyToOne
     private Employer employer;
-    private String skills;
+    @ManyToMany
+    private List<Skill> skills;
 
     public Job() {
     }
 
 
-    public Job(Employer anEmployer, String someSkills) {
+    public Job(Employer anEmployer, List<Skill> someSkills) {
         super();
         this.employer = anEmployer;
         this.skills = someSkills;
@@ -36,11 +32,11 @@ public class Job extends AbstractEntity{
         this.employer = employer;
     }
 
-    public String getSkills() {
+    public List<Skill> getSkills() {
         return skills;
     }
 
-    public void setSkills(String skills) {
+    public void setSkills(List<Skill> skills) {
         this.skills = skills;
     }
 }
